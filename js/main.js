89363977bfaa2151479c53e14264f5ef2765af9e
Alex <alexyakimenkojr@gmail.com>
2022-02-25 01:10:07 +0200

    Included Scroller module

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import Scroller from "../dist/scroller-es6.min.js";
+
 const navToggler = document.querySelector(".nav-toggler")
 const navList = document.querySelector(".nav ul")
 const navItems = navList.querySelectorAll("li")
@@ -12,3 +14,9 @@ navToggler.addEventListener("click", toggleNavMenu)
 for(let item of navItems) {
   item.addEventListener("click", toggleNavMenu)
 }
+
+new Scroller({
+  btnSelector: ".scroll-to",
+  btnAttr: "data-scroll",
+  btnPreventDefault: true
+});
