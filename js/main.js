13a7b483a4940bed62dccc40ec204cd2f0e3c69f
Alex <alexyakimenkojr@gmail.com>
2022-02-24 18:57:07 +0200

    Minor fixes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const navToggler = document.querySelector(".nav_toggler")
 const navList = document.querySelector(".nav ul")
+const navItems = navList.querySelectorAll("li")
 
-navToggler.addEventListener("click", ()=>{
+const toggleNavMenu = () => {
   navToggler.classList.toggle("fa-bars")
   navToggler.classList.toggle("fa-xmark")
   navList.classList.toggle("open")
-})
+}
+
+navToggler.addEventListener("click", toggleNavMenu)
+for(let item of navItems) {
+  item.addEventListener("click", toggleNavMenu)
+}
