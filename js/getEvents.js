73313119aca1c853e45c6b0333908e70c3e27335
Alex <alexyakimenkojr@gmail.com>
2022-03-10 20:37:15 +0200

    Redesigned events section

--- js/getEvents.js
+++ js/getEvents.js
@@ -2,15 +2,20 @@ import data from "../data/events.json" assert { type: "json" }
 
 const events = document.querySelector(".events .row")
 events.innerHTML = ""
-for (let event of data.events) {
-  events.innerHTML += `
-  <div class="col col-md-4">
-    <div class="event" style="background-image: url(${event.background})">
-      <div class="event__content">
-        <h2 class="event-title">${event.title}</h2>
-        <div class="event-description">${event.description}</div>
+if(data.events.length) {
+  for (let event of data.events) {
+    events.innerHTML += `
+    <div class="col col-md-4">
+      <div class="event" style="background-image: url(${event.background})">
+        <div class="event__content">
+          <h2 class="event-title">${event.title}</h2>
+          <i class="event-date">${event.date}</i>
+          <div class="event-description">${event.description}</div>
+        </div>
       </div>
     </div>
-  </div>
-  `
+    `
+  }
+} else {
+  events.innerHTML = "<h2 style='width: 100%'>¯\\_(ツ)_/¯</h2>"
 }
