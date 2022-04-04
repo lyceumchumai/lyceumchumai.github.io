e84d71b28cfba591abd8fbbd06b5c10c85178b33
Alex <alexyakimenkojr@gmail.com>
2022-04-04 17:57:58 +0300

    Page loading speed up

--- js/getImages.js
+++ js/getImages.js
@@ -1,18 +1,19 @@
 import data from "../data/images.json" assert { type: "json" }
 
-const gallery = document.querySelector(".gallery .row")
-gallery.innerHTML = ""
-if(data.images.length) {
-  for (let image of data.images) {
-    gallery.innerHTML += `
-    <div class="col col-md-4">
-      <a href="${image.url}" data-lity>
-        <div class="image__wrapper" style="background-image: url(${image.url})"></div>
-      </a>
-    </div>
-    `
+window.addEventListener("load", () => {
+  const gallery = document.querySelector(".gallery .row")
+  gallery.innerHTML = ""
+  if (data.images.length) {
+    for (let image of data.images) {
+      gallery.innerHTML += `
+      <div class="col col-md-4">
+        <a href="${image.url}" data-lity>
+          <div class="image__wrapper" style="background-image: url(${image.url})"></div>
+        </a>
+      </div>
+      `
+    }
+  } else {
+    gallery.innerHTML = "<h2 style='width: 100%'>¯\\_(ツ)_/¯</h2>"
   }
-} else {
-  gallery.innerHTML = "<h2 style='width: 100%'>¯\\_(ツ)_/¯</h2>"
-}
-
+})
--- js/getStaff.js
+++ js/getStaff.js
@@ -1,13 +1,15 @@
 import data from "../data/staff.json" assert { type: "json" }
 
-const table = document.querySelector(".staff-table")
-table.innerHTML = ""
+window.addEventListener("load", () => {
+  const table = document.querySelector(".staff-table")
+  table.innerHTML = ""
 
-for (let item of data.staff) {
-  table.innerHTML += `
-    <tr>
-      <td>${item.name}</td>
-      <td>${item.lesson}</td>
-    </tr>
-  `
-}
+  for (let item of data.staff) {
+    table.innerHTML += `
+      <tr>
+        <td>${item.name}</td>
+        <td>${item.lesson}</td>
+      </tr>
+    `
+  }
+})
